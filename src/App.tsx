fda991d2a8f630cdbf0bf2bd1471c8b335b82aec
igman <difariivman@gmail.com>
2026-04-25 15:01:54 +0700

    chore: new entry app

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import RootNavigator from './navigation/RootNavigator';
+import { AuthProvider } from './context/AuthContext';
+
+const App = () => {
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <AuthProvider>
+        <NavigationContainer>
+          <RootNavigator />
+        </NavigationContainer>
+      </AuthProvider>
+    </GestureHandlerRootView>
+  );
+};
+
+export default App;
